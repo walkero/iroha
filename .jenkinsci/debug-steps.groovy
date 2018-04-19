33dbd5761e2bc394b3c72eed3ff0c5276a0d78c8
tyvision <antykushin@gmail.com>
2018-04-19 21:33:56 +0300

    fix global vars (7)
    Signed-off-by: tyvision <antykushin@gmail.com>

--- .jenkinsci/debug-steps.groovy
+++ .jenkinsci/debug-steps.groovy
@@ -25,94 +25,97 @@ def doDebugBuild(coverageEnabled=false) {
   def iC = dPullOrBuild.dockerPullOrUpdate()
   // TODO: check if this works for global
   dockerAgentDockerImage = iC.imageName()
+
+  sh "echo ${dockerAgentDockerImage}" 
   // (done) TODO: save the image to the AWS EFS only in case we are only in Linux x86_64
   // TODO: check if it works
   if ("x86_64_aws_build" in env.NODE_NAME) {
     sh "docker save -o ${env.JENKINS_DOCKER_IMAGE_DIR}/${env.dockerAgentDockerImage} ${env.dockerAgentDockerImage}"
   }
-  iC.inside(""
-    + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
-    + " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
-    + " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
-    + " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
-    + " -v ${CCACHE_DIR}:${CCACHE_DIR}") {
+  // iC.inside(""
+  //   + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
+  //   + " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
+  //   + " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
+  //   + " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
+  //   + " -v ${CCACHE_DIR}:${CCACHE_DIR}") {
 
-    def scmVars = checkout scm
-    def cmakeOptions = ""
-    if ( coverageEnabled ) {
-      cmakeOptions = " -DCOVERAGE=ON "
-    }
-    env.IROHA_VERSION = "0x${scmVars.GIT_COMMIT}"
-    env.IROHA_HOME = "/opt/iroha"
-    env.IROHA_BUILD = "${env.IROHA_HOME}/build"
+  //   def scmVars = checkout scm
+  //   def cmakeOptions = ""
+  //   if ( coverageEnabled ) {
+  //     cmakeOptions = " -DCOVERAGE=ON "
+  //   }
+  //   env.IROHA_VERSION = "0x${scmVars.GIT_COMMIT}"
+  //   env.IROHA_HOME = "/opt/iroha"
+  //   env.IROHA_BUILD = "${env.IROHA_HOME}/build"
 
-    sh """
-      ccache --version
-      ccache --show-stats
-      ccache --zero-stats
-      ccache --max-size=5G
-    """  
-    sh """
-      cmake \
-        -DTESTING=ON \
-        -H. \
-        -Bbuild \
-        -DCMAKE_BUILD_TYPE=Debug \
-        -DIROHA_VERSION=${env.IROHA_VERSION} \
-        ${cmakeOptions}
-    """
-    sh "cmake --build build -- -j${parallelism}"
-    sh "ccache --show-stats"
-  }
+  //   sh """
+  //     ccache --version
+  //     ccache --show-stats
+  //     ccache --zero-stats
+  //     ccache --max-size=5G
+  //   """  
+  //   sh """
+  //     cmake \
+  //       -DTESTING=ON \
+  //       -H. \
+  //       -Bbuild \
+  //       -DCMAKE_BUILD_TYPE=Debug \
+  //       -DIROHA_VERSION=${env.IROHA_VERSION} \
+  //       ${cmakeOptions}
+  //   """
+  //   sh "cmake --build build -- -j${parallelism}"
+  //   sh "ccache --show-stats"
+  // }
 }
 
 
 def doPreCoverageStep() {
-  
+  sh "echo ${dockerAgentDockerImage}"
   if ("x86_64_aws_cov" in env.NODE_NAME) {
     sh "docker load -i ${env.JENKINS_DOCKER_IMAGE_DIR}/${env.dockerAgentDockerImage}"
   }
   def iC = docker.image("${env.dockerAgentDockerImage}")
-  iC.inside(""
-    // + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
-    // + " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
-    // + " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
-    // + " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
-    + " -v ${CCACHE_DIR}:${CCACHE_DIR}") {
+  // iC.inside(""
+  //   // + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
+  //   // + " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
+  //   // + " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
+  //   // + " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
+  //   + " -v ${CCACHE_DIR}:${CCACHE_DIR}") {
       
-      sh "cmake --build build --target coverage.init.info"
-  }
+  //     sh "cmake --build build --target coverage.init.info"
+  // }
 }
 
 // TODO: add tests list argument to pass to this function
 // now there are only unit tests running      
 def doTestStep() {
   sh "docker network create ${env.IROHA_NETWORK}"
-  docker.image('postgres:9.5').run(""
-    + " -e POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
-    + " -e POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
-    + " --name ${env.IROHA_POSTGRES_HOST}"
-    + " --network=${env.IROHA_NETWORK}")
+  sh "echo ${dockerAgentDockerImage}"
+  // docker.image('postgres:9.5').run(""
+  //   + " -e POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
+  //   + " -e POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
+  //   + " --name ${env.IROHA_POSTGRES_HOST}"
+  //   + " --network=${env.IROHA_NETWORK}")
   
   if ("x86_64_aws_test" in env.NODE_NAME) {
     sh "docker load -i ${env.JENKINS_DOCKER_IMAGE_DIR}/${env.dockerAgentDockerImage}"
   }
   def iC = docker.image("${env.dockerAgentDockerImage}")
   def path = sh(script: 'pwd', returnStdout: true);
-  iC.inside(""
-    + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
-    + " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
-    + " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
-    + " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
-    + " --network=${env.IROHA_NETWORK}"
-    + " -v ${CCACHE_DIR}:${CCACHE_DIR}") {
+  // iC.inside(""
+  //   + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
+  //   + " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
+  //   + " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
+  //   + " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
+  //   + " --network=${env.IROHA_NETWORK}"
+  //   + " -v ${CCACHE_DIR}:${CCACHE_DIR}") {
 
-      def testExitCode = sh(script: 'cmake --build build --target test', returnStatus: true)
-      if (testExitCode != 0) {
-        currentBuild.result = "UNSTABLE"
-      }
-      return
-  }
+  //     def testExitCode = sh(script: 'cmake --build build --target test', returnStatus: true)
+  //     if (testExitCode != 0) {
+  //       currentBuild.result = "UNSTABLE"
+  //     }
+  //     return
+  // }
 }
 
 
@@ -121,19 +124,19 @@ def doPostCoverageCoberturaStep() {
     sh "docker load -i ${env.JENKINS_DOCKER_IMAGE_DIR}/${env.dockerAgentDockerImage}"
   }
   def iC = docker.image("${env.dockerAgentDockerImage}")
-  iC.inside(""
-    // + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
-    // + " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
-    // + " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
-    // + " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
-    + " -v ${CCACHE_DIR}:${CCACHE_DIR}") {
+  // iC.inside(""
+  //   // + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
+  //   // + " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
+  //   // + " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
+  //   // + " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
+  //   + " -v ${CCACHE_DIR}:${CCACHE_DIR}") {
 
-        sh "cmake --build build --target coverage.info"
-        sh "python /tmp/lcov_cobertura.py build/reports/coverage.info -o build/reports/coverage.xml"
-        cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/build/reports/coverage.xml', conditionalCoverageTargets: '75, 50, 0', failUnhealthy: false, failUnstable: false, lineCoverageTargets: '75, 50, 0', maxNumberOfBuilds: 50, methodCoverageTargets: '75, 50, 0', onlyStable: false, zoomCoverageChart: false
-    }
-    // TODO: if we need to do copy built binaries to the volume each time
-    sh "cp ./build/bin/* /tmp/${GIT_COMMIT}/"
+  //       sh "cmake --build build --target coverage.info"
+  //       sh "python /tmp/lcov_cobertura.py build/reports/coverage.info -o build/reports/coverage.xml"
+  //       cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/build/reports/coverage.xml', conditionalCoverageTargets: '75, 50, 0', failUnhealthy: false, failUnstable: false, lineCoverageTargets: '75, 50, 0', maxNumberOfBuilds: 50, methodCoverageTargets: '75, 50, 0', onlyStable: false, zoomCoverageChart: false
+  //   }
+  //   // TODO: if we need to do copy built binaries to the volume each time
+  //   sh "cp ./build/bin/* /tmp/${GIT_COMMIT}/"
 }
 
 
@@ -142,27 +145,27 @@ def doPostCoverageSonarStep() {
     sh "docker load -i ${env.JENKINS_DOCKER_IMAGE_DIR}/${env.dockerAgentDockerImage}"
   }
   def iC = docker.image("${env.dockerAgentDockerImage}")
-  iC.inside(""
-    // + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
-    // + " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
-    // + " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
-    // + " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
-    + " -v ${CCACHE_DIR}:${CCACHE_DIR}") {
+  // iC.inside(""
+  //   // + " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
+  //   // + " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
+  //   // + " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
+  //   // + " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
+  //   + " -v ${CCACHE_DIR}:${CCACHE_DIR}") {
 
-      sh "cmake --build build --target cppcheck"
-      // Sonar
-      if (env.CHANGE_ID != null) {
-        sh """
-          sonar-scanner \
-            -Dsonar.github.disableInlineComments \
-            -Dsonar.github.repository='hyperledger/iroha' \
-            -Dsonar.analysis.mode=preview \
-            -Dsonar.login=${SONAR_TOKEN} \
-            -Dsonar.projectVersion=${BUILD_TAG} \
-            -Dsonar.github.oauth=${SORABOT_TOKEN} \
-            -Dsonar.github.pullRequest=${CHANGE_ID}
-        """
-      }
-  }
+  //     sh "cmake --build build --target cppcheck"
+  //     // Sonar
+  //     if (env.CHANGE_ID != null) {
+  //       sh """
+  //         sonar-scanner \
+  //           -Dsonar.github.disableInlineComments \
+  //           -Dsonar.github.repository='hyperledger/iroha' \
+  //           -Dsonar.analysis.mode=preview \
+  //           -Dsonar.login=${SONAR_TOKEN} \
+  //           -Dsonar.projectVersion=${BUILD_TAG} \
+  //           -Dsonar.github.oauth=${SORABOT_TOKEN} \
+  //           -Dsonar.github.pullRequest=${CHANGE_ID}
+  //       """
+  //     }
+  // }
 }
 return this
